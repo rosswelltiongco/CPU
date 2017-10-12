d791bac7ed7524f095749d0aaa0114d22982e7cd
rosswelltiongco <rosswelltiongco@gmail.com>
2017-10-11 17:29:16 -0700

    Added completed memory module

---
+++ Memory/Lab4_Memory_Display_MemoryOnly/ad_mux.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:30:47 10/11/2017 
+// Design Name: 
+// Module Name:    ad_mux 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ad_mux(
+    input [3:0] d7,
+    input [3:0] d6,
+    input [3:0] d5,
+    input [3:0] d4,
+    input [3:0] d3,
+    input [3:0] d2,
+    input [3:0] d1,
+    input [3:0] d0,
+    input [2:0] sel,
+    output [3:0] Y
+    );
+
+
+endmodule
---
+++ Memory/Lab4_Memory_Display_MemoryOnly/pixel_controller.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:31:55 10/11/2017 
+// Design Name: 
+// Module Name:    pixel_controller 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module pixel_controller(
+    input clk,
+    input reset,
+    output a7,
+    output a6,
+    output a5,
+    output a4,
+    output a3,
+    output a2,
+    output a1,
+    output a0,
+    output [2:0] seg_sel
+    );
+
+
+endmodule
