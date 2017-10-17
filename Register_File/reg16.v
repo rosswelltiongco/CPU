d1021285233b28e0dd54453b2bd36171b27d66b2
jelucian <jlucian995@gmail.com>
2017-10-17 10:18:26 -0700

    Created lab5 folder Register File/
    Created top module, reg16 file and added ucf file

---
+++ Register_File/reg16.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+/********************************************************************************
+ *
+ * Author:   Rosswell Tiongco & Jesus Luciano
+ * Email:    rosswelltiongco@gmail.com & jlucian995@gmail.com
+ * Filename: reg16.v
+ * Date:     
+ * Version:  1.0
+ *
+ * Description: Register File <= Add more
+ *
+ *
+ *******************************************************************************/
+module reg16(clk, reset, ld, Din, DA, DB, oeA, oeB);
+    //Declare Inputs
+    input         clk, reset, ld, oeA, oeB;
+    input [15:0]  Din;
+    
+    //Declare Registers
+    reg   [15:0]  Dout;
+
+    //Declare Outputs
+    output [15:0] DA, DB;
+
+    //Behavioral section for writing to the register
+   always @ ( posedge clk or posedge reset)
+      if(reset)
+         Dout <= 16'b0;
+      else
+         if(ld)
+            Dout <= Din;
+         else
+            Dout <= Dout;
+    
+    //conditional continuous assignments for reading the register
+    assign DA = oeA ? Dout : 16'hz;
+    assign DA = oeB ? Dout : 16'hz;
+
+
+endmodule
---
+++ Register_File/top_level_lab5.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+/********************************************************************************
+ *
+ * Author:   Rosswell Tiongco & Jesus Luciano
+ * Email:    rosswelltiongco@gmail.com & jlucian995@gmail.com
+ * Filename: top_level_lab5.v
+ * Date:     
+ * Version:  1.0
+ *
+ * Description: 
+ *
+ *
+ *******************************************************************************/
+module top_level_lab5();
+
+
+endmodule
