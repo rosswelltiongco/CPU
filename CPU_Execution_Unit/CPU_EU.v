776a75946ec34f92c3a9b958667c860f23f47272
Rosswell <rosswelltiongco@gmail.com>
2017-11-15 18:30:19 -0800

    Worked on execution unit's top level

--- CPU_Execution_Unit/CPU_EU.v
+++ CPU_Execution_Unit/CPU_EU.v
@@ -13,34 +13,36 @@
  * Work  Time: 45 mins Jesus 11/9
  * Debug Time: 
  *******************************************************************************/
-module CPU_EU(clk, reset,W_En, pc_ld, adr_sel, Din, S_Sel, pc_inc, ir_ld, address, Dout);
+module CPU_EU(clk, reset, reg_w_en, S_Sel, adr_sel, pc_ld, pc_inc, ir_ld, D_in, C, N, Z, Address, D_out);
+
    //declare inputs
-   input clk, W_En, reset, S_Sel, adr_sel, pc_ld, pc_inc, ir_ld; 
-   input [15:0] Din;
+   input clk, reset, reg_w_en, S_Sel, adr_sel, pc_ld, pc_inc, ir_ld; 
+   input [15:0] D_in;
+   
    //Din[15:12], Din[11:9], Din[8:6], Din[5:3], Din[2:0]
    //1011_           000_      001_      000_      000 <- RTL microperation
    //alu_op,         xxx,      dest,      R,        S
    
    //declare outputs
-   output [15:0] address, Dout;
+   output C, N, Z; //Flags indicated by LEDs
+   output [15:0] Address, D_out;
    
    //declare wires
-   wire C, N, Z;
    wire [2:0] W_Adr, S_Adr, R_Adr;
    wire [3:0] ALU_OP;
-   wire [15:0] IR_out, PC_out, REG_out, ALU_out;
+   wire [15:0] IR_Out, PC_Out, Reg_Out, Alu_Out;
    
 
-   //IDP    (clk, W_En, W_Adr, S_Adr, R_Adr, DS, S_Sel, ALU_OP, reset, C, N, Z, Reg_Out, Alu_out);
-   IDP  idp (clk, W_En, W_Adr, S_Adr, R_Adr, DS, S_Sel, ALU_OP, reset, C, N, Z, reg_out, Alu_out);
+   //IDP    (clk,    W_En, W_Adr, S_Adr, R_Adr,   DS, S_Sel, ALU_OP, reset, C, N, Z, Reg_Out, Alu_out);
+   IDP  idp (clk, reg_w_en, W_Adr, S_Adr, R_Adr, D_in, S_Sel, ALU_OP, reset, C, N, Z, Reg_Out, Alu_Out);
 
    
    //reg16_L_Inc (clk, reset,    ld,    inc,       D,      q);
-   reg16_L_Inc PC(clk, reset, pc_ld, pc_inc, ALU_out, PC_out);
+   reg16_L_Inc PC(clk, reset, pc_ld, pc_inc, Alu_Out, PC_Out);
    
    //reg16_L_Inc (clk, reset,    ld,  inc,    D,      q);
-   reg16_L_Inc IR(clk, reset, ir_ld, 1'b0, D_in, IR_out);
+   reg16_L_Inc IR(clk, reset, ir_ld, 1'b0, D_in, IR_Out);
 
-   assign Address = adr_sel ? REG_out : PC_out;
+   assign Address = adr_sel ? Reg_Out: PC_Out;
 
 endmodule
--- CPU_Execution_Unit/top_level_lab7.v
+++ CPU_Execution_Unit/top_level_lab7.v
@@ -13,24 +13,39 @@
  * Work  Time: 45 mins Jesus 11/9
  * Debug Time: 
  *******************************************************************************/
-module top_level_lab7(clk, reset, );
+module top_level_lab7(clk, reset, step_clk, adr_sel, mem_w_en, reg_w_en, S_Sel, pc_ld, pc_inc, ir_ld, 
+                      anode, a, b, c, d, e, f, g, C, N, Z);
 
-   //input 
+   //inputs
+   input clk, reset, step_clk, adr_sel, mem_w_en, reg_w_en, S_Sel, pc_ld, pc_inc, ir_ld;
    
-   output 
+   //outputs
+   output C, N, Z;
+   output a, b, c, d, e, f, g;
+   output [7:0] anode;
    
-   //CPU_EU
-   //IDP shoares input with IR
-   CPU_EU execution_unit();
+   //wires
+   wire step_clk_out, w_en_out;
+   //wire [15:0] RAM_Out, CE_Out, CE_Adr
+   wire [15:0] mem_d_out, eu_d_out, address;
    
    //debounce   (clk, reset,      Din,         Dout);
    debounce step(clk, reset, step_clk, step_clk_out);
    
-   //debounce               (clk, reset,      Din,         Dout);
-   debounce mem_write_enable(clk, reset, mem_w_en, mem_w_en_out);
    
-   //ram7          (clk,           we,    addr,  din,  dout);
+   //debounce               (clk, reset,      Din, Dout);
+   debounce mem_write_enable(clk, reset, mem_w_en, w_en_out);
+   
+   
+   //CPU_EU
+   //IDP shares input with IR
+   //                   (clk,          reset, W_En, S_Sel, adr_sel, pc_ld, pc_inc, ir_ld, D_in     , C, N, Z, Address, D_out);
+   CPU_EU execution_unit(step_clk_out, reset, W_En, S_Sel, adr_sel, pc_ld, pc_inc, ir_ld, mem_d_out, C, N, Z, address, eu_d_out);
+   
+   
+   //ram7          (clk,           we,    addr,      din,  dout);
    ram7 main_memory(clk, mem_w_en_out, address, eu_d_out, mem_d_out);
+
 
    //Display_Controller   (clk, reset, 
    Display_Controller disp(clk, reset,
