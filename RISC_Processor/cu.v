92304b2568734270da76f0dd9220e3a9e8b5a8b5
jelucian <jlucian995@gmail.com>
2017-11-22 23:00:24 -0800

    Created Lab 8 folder and project

---
+++ RISC_Processor/cu.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:59:06 11/22/2017 
+// Design Name: 
+// Module Name:    cu 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module cu(
+    );
+
+
+endmodule
